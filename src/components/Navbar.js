55df38a8945f1f7f50a0c5975f81a06af65f1d9c
bartek9105 <bartek9105@gmail.com>
2021-06-22 12:03:45 +0200

    Create navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+export const Navbar = () => {
+  return (
+    <nav className="flex justify-between p-8 max-w-screen-2xl m-auto">
+      <span>Logo</span>
+      <ul class="flex">
+        <li className="mr-12 cursor-pointer text-white hover:text-red-600 transition delay-100 ease-in-out">Movies</li>
+        <li className="mr-12 cursor-pointer text-white hover:text-red-600 transition delay-100 ease-in-out">TV Shows</li>
+        <li className="cursor-pointer text-white hover:text-red-600 transition delay-100 ease-in-out">Actors</li>
+      </ul>
+    </nav>
+  )
+}
